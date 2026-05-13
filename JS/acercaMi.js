35b3d1f4f0d7f82e85036e753777842643fcbdf5
LusellyHinestroza <valencialucelly48@gmail.com>
2026-05-13 01:12:29 -0500

    actualizar

---
+++ JS/acercaMi.js
@@ -0,0 +1,7 @@
+function irEnlaces(url){
+    window.open(url, "_blank")
+}
+
+function menuResponse(){
+    document.getElementById("menu").classList.toggle("active");
+}
---
+++ JS/navbar.js
@@ -0,0 +1,8 @@
+function menuResponse(){
+    document.getElementById("menu").classList.toggle("active");
+    document.getElementById("menu-resP").classList.toggle("active");
+}
+
+function irEnlaces(url){
+    window.open(url, "_blank")
+}
