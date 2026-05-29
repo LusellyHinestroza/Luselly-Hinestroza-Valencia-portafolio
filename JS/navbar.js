be93ae16b829ccdf737480c8a806fd376686181f
LusellyHinestroza <valencialucelly48@gmail.com>
2026-05-29 15:51:11 -0500

    actulizar

--- JS/navbar.js
+++ JS/navbar.js
@@ -7,3 +7,9 @@ function irEnlaces(url){
     window.open(url, "_blank")
 }
 
+document.addEventListener("DOMContentLoaded", async () => {
+    const response = await fetch("../html/navbar.html");
+    const html = await response.text();
+
+    document.getElementById("navbar-container").innerHTML = html;
+});
